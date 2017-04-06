850cef6107f029cf55624d7a977f74ef1093406e
Vishal Bhatnagar <vishal.bhatnagar.14@gmail.com>
2017-04-05 20:52:46 -0400

    Generic Xcode commit before system restore

---
+++ CocoaProjects/RandomPassword/RandomPassword/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  RandomPassword
+//
+//  Created by Vishal Bhatnagar on 5/29/16.
+//  Copyright © 2016 Vishal Bhatnagar. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
